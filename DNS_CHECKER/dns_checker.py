4a27e2119cd8726ba6dd9cb26d993631594358f5
iiihenrymorrin <henry.morrin@iii.com>
2019-03-22 15:44:43 +0000

    Saving searches to file

--- DNS_CHECKER/dns_checker.py
+++ DNS_CHECKER/dns_checker.py
@@ -7,23 +7,36 @@ import dns.resolver
 
 #Define the inputs
 tiers= {"App": "" , "DB" : "-db", "Encore" : "-encore" , "MT" : "-mt"} #Tiers of sitecode I should query - I will use a dict
-sitecode=str(raw_input("Enter a sitcode: ")) #Get the customer sitecode from user
 domain=".iii.com" #Domain that looking for Records for.
 Resolvers= ["8.8.8.8","10.128.129.25"] # DNS resolvers to query list
+records_list=["A","TXT"]
+wildcard="*."
 
 
 #Define the functions that will take inputs and do necessary checks
-def resolve_fqdn_A():
+def resolve_fqdn_records(my_resolver,resolver,sitecode):
+    open_file=open(sitecode+".txt", "w")
     for tier in tiers:
             try:
-                fqdn=str(sitecode+tier+domain)
-                ip_addr=socket.gethostbyname(sitecode+tier+domain)
-                print("The IP address for %s%s%s is : %s" % (sitecode,tier,domain, ip_addr))
-            except:
-                print("%s%s%s did not resolve correctly" % (sitecode,tier,domain))
-                continue #Move onto the next index in the list
-#call the functions with input data and out put results
+                answer=my_resolver.query(sitecode+tiers[tier]+domain, "A")
+                for data in answer:
+                        print "The DNS resolution of %s%s%s is %s from %s " % (sitecode,tiers[tier],domain, data.address,resolver)
+                        open_file.write("The DNS resolution of %s%s%s is %s from %s \n" % (sitecode,tiers[tier],domain, data.address,resolver))
+                answer=my_resolver.query(sitecode+tiers[tier]+domain, "TXT")
+                for data in answer:
+                        print "The DNS resolution of %s%s%s is %s from %s " % (sitecode,tiers[tier],domain, data.to_text(),resolver)
+                        open_file.write("The DNS resolution of %s%s%s is %s from %s \n " % (sitecode,tiers[tier],domain, data.address,resolver))
+                if tiers[tier] == "":
+                    answer=my_resolver.query(wildcard+sitecode+tiers[tier]+domain, "A")
+                    for data in answer:
+                            print "The DNS resolution of %s%s%s%s is %s from %s " % (wildcard,sitecode,tiers[tier],domain, data.address,resolver)
+                            open_file.write("The DNS resolution of %s%s%s%s is %s from %s " % (wildcard,sitecode,tiers[tier],domain, data.address,resolver))
 
+            except:
+                print "No dns entry for %s%s%s" % (sitecode,tiers[tier],domain)
+                open_file.write("No dns entry for %s%s%s \n" % (sitecode,tiers[tier],domain))
+                continue
+    open_file.close()
 
 
 def set_resolver(dns_resolver):
@@ -31,13 +44,11 @@ def set_resolver(dns_resolver):
     my_resolver.nameservers = [dns_resolver]
     return my_resolver
 
-
 def main():
+    sitecode=str(raw_input("Enter a sitcode: "))
     for resolver in Resolvers:
         my_resolver=set_resolver(resolver)
-        answer=my_resolver.query('google.com', "A")
-        for data in answer:
-            print "The ip address as resolved by the dns resolvers %s is: %s" % ( resolver, data.address)
+        resolutions=resolve_fqdn_records(my_resolver,resolver,sitecode)
 
 
 main()
