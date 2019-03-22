0421f9113cd4dd7bce331f686f7850ac0ad71d84
iiihenrymorrin <henry.morrin@iii.com>
2019-03-22 17:23:13 +0000

    Dumping results to json file for parsing for sanity checks

--- DNS_CHECKER/dns_checker.py
+++ DNS_CHECKER/dns_checker.py
@@ -3,40 +3,45 @@
 #Define the Modules I will use
 import socket
 import dns.resolver
+import json
 
 
 #Define the inputs
 tiers= {"App": "" , "DB" : "-db", "Encore" : "-encore" , "MT" : "-mt"} #Tiers of sitecode I should query - I will use a dict
 domain=".iii.com" #Domain that looking for Records for.
-Resolvers= ["8.8.8.8","10.128.129.25"] # DNS resolvers to query list
+Resolvers= {"8.8.8.8": "_External","10.128.129.25": "_Internal"} # DNS resolvers to query list
 records_list=["A","TXT"]
 wildcard="*."
+reverse_domain=".in-addr.arpa"
 
 
 #Define the functions that will take inputs and do necessary checks
-def resolve_fqdn_records(my_resolver,resolver,sitecode):
-    open_file=open(sitecode+".txt", "w")
+
+def resolve_fqdn_records(my_resolver,resolver,sitecode,resolutions):
     for tier in tiers:
             try:
                 answer=my_resolver.query(sitecode+tiers[tier]+domain, "A")
                 for data in answer:
                         print "The DNS resolution of %s%s%s is %s from %s " % (sitecode,tiers[tier],domain, data.address,resolver)
-                        open_file.write("The DNS resolution of %s%s%s is %s from %s \n" % (sitecode,tiers[tier],domain, data.address,resolver))
+                        resolutions[tier+"_IP"+Resolvers[resolver]] = data.address
+                        print resolutions
+
+
                 answer=my_resolver.query(sitecode+tiers[tier]+domain, "TXT")
                 for data in answer:
                         print "The DNS resolution of %s%s%s is %s from %s " % (sitecode,tiers[tier],domain, data.to_text(),resolver)
-                        open_file.write("The DNS resolution of %s%s%s is %s from %s \n " % (sitecode,tiers[tier],domain, data.address,resolver))
+                        resolutions[tier+"_TXT"+Resolvers[resolver]] = data.to_text()
+
                 if tiers[tier] == "":
                     answer=my_resolver.query(wildcard+sitecode+tiers[tier]+domain, "A")
                     for data in answer:
                             print "The DNS resolution of %s%s%s%s is %s from %s " % (wildcard,sitecode,tiers[tier],domain, data.address,resolver)
-                            open_file.write("The DNS resolution of %s%s%s%s is %s from %s " % (wildcard,sitecode,tiers[tier],domain, data.address,resolver))
+                            resolutions[tier+"_Wildcard"+Resolvers[resolver]] = data.address
 
             except:
                 print "No dns entry for %s%s%s" % (sitecode,tiers[tier],domain)
-                open_file.write("No dns entry for %s%s%s \n" % (sitecode,tiers[tier],domain))
                 continue
-    open_file.close()
+    return resolutions
 
 
 def set_resolver(dns_resolver):
@@ -46,9 +51,13 @@ def set_resolver(dns_resolver):
 
 def main():
     sitecode=str(raw_input("Enter a sitcode: "))
+    resolutions={}
     for resolver in Resolvers:
         my_resolver=set_resolver(resolver)
-        resolutions=resolve_fqdn_records(my_resolver,resolver,sitecode)
+        data=resolve_fqdn_records(my_resolver,resolver,sitecode,resolutions)
+    with open(sitecode+".json", "w") as f:
+        json.dump(data, f)
+    print "Created json file"
 
 
 main()
